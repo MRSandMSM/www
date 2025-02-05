946cb811b312b4ebc27ace59c7f5600ce79e79b9
MRSandMSM <MRSandMSM@gmail.com>
2025-02-05 14:11:41 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MR S and MS M</title>
+  </head>
+  <body>
+    <div id="pageContainer">
+      <header id="header">
+        <div class="header-content">
+          <h1 class="logo">
+            <a href="/">MR S and MS M</a>
+          </h1>
+          <a href="#" class="mobile-nav-toggler">
+            <span class="bar bar-top"></span>
+            <span class="bar bar-middle"></span>
+            <span class="bar bar-bottom"></span>
+            <span class="text">Menu</span>
+          </a>
+        </div>
+        <div class=""></div>
+      </header>
+      <main id="page">
+        <?php echo 'Hello world'; ?>
+      </main>
+      <footer id="footer">
+        <div class="section" id="footerCopyright">
+          <ul class="menu" id="copyrightMenu">
+            <li>&copy; <?php echo date("Y"); ?> MR S and MS M.</li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/privacy">Privacy</a></li>
+            <li><a href="/terms">Terms</a></li>
+            <li><a href="/cookies">Cookies</a></li>
+            <li><a href="/ccpa">CCPA</a></li>
+            <li>
+              <a href="#siteCredits" class="credits">Credits</a>
+              <div id="siteCredits">
+                <p>Site designed by <a href="/about/band/mr-s">the nosey raccoon</a> and <a href="/about/band/ms-m">the tortured turtle</a>.</p>
+                <p>Built in NYC by the dev team @ <a href="https://www.valenselectronsmgmt.com" target="_blank">Valens Electrons</a></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
