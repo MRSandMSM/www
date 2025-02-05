a2830afdb3480c9a8812b612de1cc9d01febc687
MRSandMSM <MRSandMSM@gmail.com>
2025-02-05 17:07:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MR S and MS M</title>
+    <link rel="stylesheet" href="/assets/css/main.css">
   </head>
   <body>
     <div id="pageContainer">
