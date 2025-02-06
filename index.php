35e01a1cf331838a60f981c5df58bb0d13d4e136
MRSandMSM <MRSandMSM@gmail.com>
2025-02-05 21:31:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,38 +7,16 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MR S and MS M</title>
+    <title>MR S and MS M | Official Website</title>
     <link rel="stylesheet" href="/assets/css/main.css">
   </head>
   <body class="home <?php echo $activeTheme; ?>">
     <div id="pageContainer">
-      <header id="header">
-        <div class="header-content">
-          <h1 class="logo">
-            <a href="/">MR S and MS M</a>
-          </h1>
-          <a href="#" class="mobile-nav-toggler">
-            <span class="bar bar-top"></span>
-            <span class="bar bar-middle"></span>
-            <span class="bar bar-bottom"></span>
-            <span class="text">Menu</span>
-          </a>
-        </div>
-        <nav class="navigation">
-          <ul class="nav" id="siteNavigation">
-            <li class="nav-item">
-              <a href="/news" class="nav-link">News</a>
-            </li>
-            <li class="nav-item last">
-              <a href="/store" class="nav-link">Store</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <?php include_once __DIR__ . '/includes/hd.php' ?>
       <main id="page">
         <?php echo 'this is the website.'; ?>
       </main>
-      <?php include_once '/includes/ft.php' ?>
+      <?php include_once __DIR__ . '/includes/ft.php' ?>
     </div>
   </body>
 </html>
