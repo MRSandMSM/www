64b6d1811b88a5850f3fe1b3c2b833787007d4ef
MRSandMSM <MRSandMSM@gmail.com>
2025-02-06 00:40:16 -0300

    Update hd.php

--- includes/hd.php
+++ includes/hd.php
@@ -1,23 +1,25 @@
 <header id="header">
-        <div class="header-content">
-          <h1 class="logo">
-            <a href="/">MR S and MS M</a>
-          </h1>
-          <a href="#" class="mobile-nav-toggler">
-            <span class="bar bar-top"></span>
-            <span class="bar bar-middle"></span>
-            <span class="bar bar-bottom"></span>
-            <span class="text">Menu</span>
-          </a>
+        <div class="header-inner">
+          <div class="header-content">
+            <h1 class="logo">
+              <a href="/">MR S and MS M</a>
+            </h1>
+            <a href="#" class="mobile-nav-toggler">
+              <span class="bar bar-top"></span>
+              <span class="bar bar-middle"></span>
+              <span class="bar bar-bottom"></span>
+              <span class="text">Menu</span>
+            </a>
+          </div>
+          <nav class="navigation">
+            <ul class="nav" id="siteNavigation">
+              <li class="nav-item">
+                <a href="/news" class="nav-link">News</a>
+              </li>
+              <li class="nav-item last">
+                <a href="/store" class="nav-link">Store</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav class="navigation">
-          <ul class="nav" id="siteNavigation">
-            <li class="nav-item">
-              <a href="/news" class="nav-link">News</a>
-            </li>
-            <li class="nav-item last">
-              <a href="/store" class="nav-link">Store</a>
-            </li>
-          </ul>
-        </nav>
       </header>
