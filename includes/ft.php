da9b52d543e9bbda038ba8e24ad1ce5874a88236
MRSandMSM <MRSandMSM@gmail.com>
2025-02-05 23:46:42 -0300

    Update ft.php

--- includes/ft.php
+++ includes/ft.php
@@ -1,33 +1,33 @@
 <footer id="footer">
-      <div class="footer-inner">
-         <div class="section" id="footerCopyright">
-           <ul class="nav" id="copyrightMenu">
-             <li class="nav-item">
-               <span class="nav-span">&copy; <?php echo date("Y"); ?> MR S and MS M</span>
-             </li>
-             <li class="nav-item">
-               <a href="/contact" class="nav-link">Contact</a>
-             </li>
-             <li class="nav-item">
-               <a href="/privacy" class="nav-link">Privacy</a>
-             </li>
-             <li class="nav-item">
-               <a href="/terms" class="nav-link">Terms</a>
-             </li>
-             <li class="nav-item">
-               <a href="/cookies" class="nav-link">Cookies</a>
-             </li>
-             <li class="nav-item">
-               <a href="/ccpa" class="nav-link">CCPA</a>
-             </li>
-             <li class="nav-item">
-               <a href="#siteCredits" class="nav-link credits">Credits</a>
-               <div id="siteCredits">
-                 <p>Site designed by <a href="/about/band/mr-s">the nosey raccoon</a> and <a href="/about/band/ms-m">the tortured turtle</a>.</p>
-                 <p>Built in NYC by the dev team @ <a href="https://www.valenselectronsmgmt.com" target="_blank">Valens Electrons</a></p>
-               </div>
-             </li>
-           </ul>
-         </div>
-       </div>
-     </footer>
+        <div class="footer-inner">
+          <div class="section" id="footerCopyright">
+            <ul class="nav" id="copyrightMenu">
+              <li class="nav-item">
+                <span class="nav-span">&copy; <?php echo date("Y"); ?> MR S and MS M</span>
+              </li>
+              <li class="nav-item">
+                <a href="/contact" class="nav-link">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a href="/privacy" class="nav-link">Privacy</a>
+              </li>
+              <li class="nav-item">
+                <a href="/terms" class="nav-link">Terms</a>
+              </li>
+              <li class="nav-item">
+                <a href="/cookies" class="nav-link">Cookies</a>
+              </li>
+              <li class="nav-item">
+                <a href="/ccpa" class="nav-link">CCPA</a>
+              </li>
+              <li class="nav-item">
+                <a href="#siteCredits" class="nav-link credits">Credits</a>
+                <div id="siteCredits">
+                  <p>Site designed by <a href="/about/band/mr-s">the nosey raccoon</a> and <a href="/about/band/ms-m">the tortured turtle</a>.</p>
+                  <p>Built in NYC by the dev team @ <a href="https://www.valenselectronsmgmt.com" target="_blank">Valens Electrons</a></p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
