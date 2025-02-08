78a01abd7644c0eeb713170c8675f7fc31d82031
MRSandMSM <MRSandMSM@gmail.com>
2025-02-08 17:49:17 -0300

    Update scripts.php

--- includes/function/scripts.php
+++ includes/function/scripts.php
@@ -1,10 +1,10 @@
 <script src="/assets/js/jquery-1.8.3.min.js"></script>
-<script src="/assets/js/scripts.js"></script>
-<?php
-$request_uri = $_SERVER['REQUEST_URI'];
-if ($request_uri == '/' || $request_uri == '/index.php') {
-  echo '<script src="/assets/js/homepage.js"></script>';
-} else {
-  echo '<script src="/assets/js/interiors.js"></script>';
-}
-?>
+    <script src="/assets/js/scripts.js"></script>
+    <?php
+    $request_uri = $_SERVER['REQUEST_URI'];
+    if ($request_uri == '/' || $request_uri == '/index.php') {
+      echo '<script src="/assets/js/homepage.js"></script>';
+    } else {
+      echo '<script src="/assets/js/interiors.js"></script>';
+    }
+    ?>
