9d33415e206718a648882462090f08b60dbf33a6
MRSandMSM <MRSandMSM@gmail.com>
2025-02-08 19:58:07 -0300

    Update navs.php

--- includes/function/navs.php
+++ includes/function/navs.php
@@ -1,10 +1,10 @@
 <?php
-function build_menu($items) {
+function build_menu($items, $id = null) {
     // Get the current page URL
     $current_url = $_SERVER['REQUEST_URI'];
 
     // Open the unordered list tag
-    $menu = '<ul class="menu">';
+    $menu = '<ul class="nav"' . ($id ? ' id="' . htmlspecialchars($id) . '"' : '') . '>';
 
     // Loop through each item in the items array
     foreach ($items as $item) {
@@ -15,16 +15,13 @@ function build_menu($items) {
             $menu .= '<a href="' . $item['url'] . '">' . $item['label'] . '</a>';
             $menu .= '<ul class="ultramenu">';
 
-            // Loop through the sub-menu items
             foreach ($item['sub_menu'] as $sub_item) {
-                // Add active class if the sub-item matches the current URL
                 $menu .= '<li' . (strpos($current_url, $sub_item['url']) !== false ? ' class="active"' : '') . '>';
                 $menu .= '<a href="' . $sub_item['url'] . '" class="nav-item">' . $sub_item['label'] . '</a></li>';
             }
 
             $menu .= '</ul></li>';
         } else {
-            // If there's no sub-menu, simply create a list item with a link
             $menu .= '<li class="nav-item' . (strpos($current_url, $item['url']) !== false ? ' active' : '') . '">';
             $menu .= '<a href="' . $item['url'] . '" class="nav-item">' . $item['label'] . '</a></li>';
         }
