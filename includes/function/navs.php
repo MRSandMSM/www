fb8f06d9bcba099174b87513d3dea161783da2a5
MRSandMSM <MRSandMSM@gmail.com>
2025-02-11 12:22:37 -0300

    Update navs.php

--- includes/function/navs.php
+++ includes/function/navs.php
@@ -12,18 +12,18 @@ function build_menu($items, $id = null) {
         if (isset($item['sub_menu']) && is_array($item['sub_menu'])) {
             // If there's a sub-menu, make it a dropdown
             $menu .= '<li class="nav-item has-ultramenu' . (strpos($current_url, $item['url']) !== false ? ' active' : '') . '">';
-            $menu .= '<a href="' . $item['url'] . '">' . $item['label'] . '</a>';
+            $menu .= '<a href="' . $item['url'] . '" class="nav-link">' . $item['label'] . '</a>';
             $menu .= '<ul class="ultramenu">';
 
             foreach ($item['sub_menu'] as $sub_item) {
                 $menu .= '<li' . (strpos($current_url, $sub_item['url']) !== false ? ' class="active"' : '') . '>';
-                $menu .= '<a href="' . $sub_item['url'] . '" class="nav-item">' . $sub_item['label'] . '</a></li>';
+                $menu .= '<a href="' . $sub_item['url'] . '" class="nav-link">' . $sub_item['label'] . '</a></li>';
             }
 
             $menu .= '</ul></li>';
         } else {
             $menu .= '<li class="nav-item' . (strpos($current_url, $item['url']) !== false ? ' active' : '') . '">';
-            $menu .= '<a href="' . $item['url'] . '" class="nav-item">' . $item['label'] . '</a></li>';
+            $menu .= '<a href="' . $item['url'] . '" class="nav-link">' . $item['label'] . '</a></li>';
         }
     }
 
