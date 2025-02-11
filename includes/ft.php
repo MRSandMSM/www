7cdf29a9d03cd4b6581f8bba15fcedd1a98f587a
MRSandMSM <MRSandMSM@gmail.com>
2025-02-11 14:45:40 -0300

    Update ft.php

--- includes/ft.php
+++ includes/ft.php
@@ -5,29 +5,6 @@
               <p>&copy; <?php echo date("Y"); ?> MR S and MS M.</p>
             </div>
             <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/navs/footer.php'; ?>
-            <ul class="nav" id="copyrightNav">
-              <li class="nav-item">
-                <a href="/contact" class="nav-link">Contact</a>
-              </li>
-              <li class="nav-item">
-                <a href="/privacy" class="nav-link">Privacy</a>
-              </li>
-              <li class="nav-item">
-                <a href="/terms" class="nav-link">Terms</a>
-              </li>
-              <li class="nav-item last">
-                <a href="#siteCredits" class="nav-link credits">Credits</a>
-                <div id="siteCredits">
-                  <a href="#" class="close">
-                    <span class="visually-hidden">Close</span>
-                  </a>
-                  <div class="credits-inner">
-                    <p>Site designed by <a href="/about/band/mr-s">the nosey raccoon</a> and <a href="/about/band/ms-m">the tortured turtle</a>.</p>
-                    <p>Built in NYC by the dev team @ <a href="https://www.valenselectronsmgmt.com" target="_blank">Valens Electrons</a></p>
-                  </div>
-                </div>
-              </li>
-            </ul>
           </div>
         </div>
       </footer>
